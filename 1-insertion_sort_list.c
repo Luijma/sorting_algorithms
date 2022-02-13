e880652b7f5f302f7fe79ecd6596e9f0c4230ae2
Luijma <rsluismanuel@gmail.com>
2022-02-13 23:55:36 +0000

    T1 A1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,5 +5,57 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *result = NULL;
+	listint_t *current = list;
+	listint_t *next;
 
+	while (current)
+	{
+		next = current->next;		
+		current->prev = current->next;
+
+		insert(&result, current);
+
+		current = next;
+	}
+
+	*list = result;
+}
+/**
+ * insert - helper function for insertion alg
+ * @head: head of the sorted version of the list
+ * @new: the new node that will be inserted
+ */
+void insert(listint_t **head, listint_t *new)
+{
+	listint_t *current;
+
+	if (!head)
+	{
+		*head = new;
+	} else if ((*head)->n >= new->n)
+	{
+		new->next = *head;
+		new->next->prev = new;
+		*head = new;
+	} else
+	{
+		current = *head;
+
+		while (current->next != NULL &&
+			current->next->n < new->n)
+		{
+			current = current->next;
+		}
+
+		new->next = current->next;
+
+		if (current->next != NULL)
+		{
+			new->next->prev = new;
+		}
+
+		current->next = new;
+		new->prev = current;
+	}
 }
