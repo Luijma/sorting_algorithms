7fe95ac526761f2624b444cd6523c163bcf83396
Luijma <rsluismanuel@gmail.com>
2022-02-13 01:49:01 +0000

    T0 A1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,5 +6,36 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	int iter = 0;
+	int cur = 0;
+	int flag;
 
+	for (iter = 0; iter < (size - 1); iter++)
+	{
+		flag = 0;
+		for (cur = 0; cur < (size - iter - 1); cur++)
+		{
+			if (array[cur] > array[cur + 1])
+			{
+				swap_elements(&array[cur], &array[cur + 1]);
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
+/**
+ * swap_elements - swaps two adjacent elements
+ * @a: smaller element
+ * @b: bigger element
+ */
+void swap_elements(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
