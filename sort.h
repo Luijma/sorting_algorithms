948ac99484f18225559c282968598678744dd94b
Luijma <rsluismanuel@gmail.com>
2022-02-13 23:54:10 +0000

    Adding library

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -25,5 +25,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_elements(int *a, int *b);
+void insert(listint_t **head, listint_t *new);
 
 #endif /** SORT_H **/
