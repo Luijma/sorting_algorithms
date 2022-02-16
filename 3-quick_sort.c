062ac397a8f372d29bf9b0256f072f4856d8c2eb
Luijma <rsluismanuel@gmail.com>
2022-02-16 00:07:58 +0000

    This wil fail some checkers

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,5 +6,64 @@
  */
 void quick_sort(int *array, size_t size)
 {
+	recursive_sort(array, 0,((int)size) - 1, size);
+}
+/**
+ * swap_elements - swaps two adjacent elements
+ * @a: smaller element
+ * @b: bigger element
+ */
+void swap_elements(int *a, int *b)
+{
+        int temp;
 
+        temp = *a;
+        *a = *b;
+        *b = temp;
+}
+/**
+ * recursive_sort - performs recursion to do quick sort
+ * @array: array to be sorted
+ * @low: low number
+ * @high: higher number
+ */
+void recursive_sort(int *array, int low, int high, size_t size)
+{
+	int prt;
+
+	if (low < high)
+	{
+		prt = partition(array, low, high, size);
+
+		recursive_sort(array, low, prt - 1, size);
+
+		recursive_sort(array, prt + 1, high, size);
+	}
+}
+/**
+ * create_partition - finds position of partition
+ * @array: array to look through
+ * @low: used for pointer for greater element
+ * @high: used to find end
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+
+			swap_elements(&array[i], &array[j]);
+		}
+	}
+
+	swap_elements(&array[i + 1], &array[high]);
+	print_array(array, size);
+
+	return (i + 1);
 }
